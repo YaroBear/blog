c584b8d66b8a23ecb6bf2d0e98e8c6016b376b4b
YaroBear <berejnoi.yaroslav@gmail.com>
2023-08-31 20:37:00 -0600

    add sagitta and inscribed triangle geometry overlay

--- demos/archimedes-principle.js
+++ demos/archimedes-principle.js
@@ -10,8 +10,41 @@ let massBall = 10; // kg
 let canvas;
 let ctx;
 
+let sectorAngle = 0;
+
+let geometryOverlayParams = {
+    'sagitta': {
+        'points': [
+            {
+                'x': 0,
+                'y': 0
+            },
+            {
+                'x': 0,
+                'y': 0
+            }
+        ]
+    },
+    'inscribedTriangle': {
+        'points': [
+            {
+                'x': 0,
+                'y': 0
+            },
+            {
+                'x': 0,
+                'y': 0
+            },
+            {
+                'x': 0,
+                'y': 0
+            }
+        ]
+    }
+}
+
 const drawWater = (x, y, w, h) => {
-    ctx.fillStyle = "rgba(0, 0, 255, .8)";
+    ctx.fillStyle = "rgba(0, 0, 255, .6)";
     ctx.fillRect(x, y, w, h);
 };
 
@@ -23,6 +56,64 @@ const drawBall = (x, y) => {
     ctx.fill();
 };
 
+const drawGeometryOverlay = () => {
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+
+    const sagittaPoints = geometryOverlayParams['sagitta'].points;
+    ctx.beginPath();
+    ctx.moveTo(sagittaPoints[0].x, sagittaPoints[0].y);
+    ctx.lineTo(sagittaPoints[1].x, sagittaPoints[1].y);
+    ctx.stroke();
+
+    const inscribedTrianglePoints = geometryOverlayParams['inscribedTriangle'].points;
+    ctx.beginPath();
+    ctx.moveTo(inscribedTrianglePoints[0].x, inscribedTrianglePoints[0].y);
+    ctx.lineTo(inscribedTrianglePoints[1].x, inscribedTrianglePoints[1].y);
+    ctx.lineTo(inscribedTrianglePoints[2].x, inscribedTrianglePoints[2].y);
+    ctx.lineTo(inscribedTrianglePoints[0].x, inscribedTrianglePoints[0].y);
+    ctx.stroke();
+
+    ctx.font = "16px serif";
+    ctx.fillText(`${(sectorAngle * 180 / Math.PI).toFixed(2)} °`, inscribedTrianglePoints[2].x, inscribedTrianglePoints[2].y);
+}
+
+const updateGeometryOverlaySagitta = (diskSagittaLength) => {
+    const canvasCenterX = canvas.width / 2;
+    const canvasCenterY = canvas.height / 2;
+    geometryOverlayParams['sagitta'].points = [
+        {
+            x: canvasCenterX,
+            y: canvasCenterY
+        },
+        {
+            x: canvasCenterX,
+            y: canvasCenterY + diskSagittaLength
+        }
+    ];
+}
+
+const updateGeometryOverlayInscribedAngle = () => {
+    const canvasCenterX = canvas.width / 2;
+    const canvasCenterY = canvas.height / 2;
+    const triangleBaseLength = radiusBall * Math.sin(sectorAngle / 2);
+    const d = radiusBall * Math.cos(sectorAngle / 2);
+
+    geometryOverlayParams['inscribedTriangle'].points = [
+        {
+            x: canvasCenterX - triangleBaseLength,
+            y: canvasCenterY
+        },
+        {
+            x: canvasCenterX + triangleBaseLength,
+            y: canvasCenterY
+        },
+        {
+            x: canvasCenterX,
+            y: canvasCenterY - d
+        }
+    ];
+}
+
 const calculatePercentageSubmerged = () => {
     const percentageSubmerged = densityBall / densityWater;
     if (percentageSubmerged > 1) {
@@ -31,13 +122,13 @@ const calculatePercentageSubmerged = () => {
     return percentageSubmerged;
 };
 
-const calculateDiskSagitta = () => {
+const calculateDiskSagittaLength = () => {
     // sagitta: h = r - rcos(theta/2)
     // sector area/circle area = sector angle/circle angle
     // so, % area submerged = % circle angle
-    const sectorAngle = calculatePercentageSubmerged() * 2 * Math.PI;
-    const sagitta = radiusBall - radiusBall * Math.cos(sectorAngle/2);
-    return sagitta;
+    sectorAngle = calculatePercentageSubmerged() * 2 * Math.PI;
+    const sagittaLength = radiusBall - radiusBall * Math.cos(sectorAngle / 2);
+    return sagittaLength;
 };
 
 const setDensityOfBall = () => {
@@ -86,9 +177,14 @@ const conditionallyShowBuoyancyNote = () => {
 
 const drawScene = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const diskSagitta = calculateDiskSagitta();
-    drawBall(canvas.width / 2, canvas.height / 2 - radiusBall + diskSagitta);
+    const diskSagittaLength = calculateDiskSagittaLength();
+    const ballCenterX = canvas.width / 2;
+    const ballCenterY = canvas.height / 2 - radiusBall + diskSagittaLength;
+    updateGeometryOverlaySagitta(diskSagittaLength);
+    updateGeometryOverlayInscribedAngle();
+    drawBall(ballCenterX, ballCenterY);
     drawWater(0, canvas.height, canvas.width, canvas.height / -2);
+    drawGeometryOverlay();
 };
 
 const onChanges = () => {
